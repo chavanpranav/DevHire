ce8a9142dd7afb52b33544fce382ea94383eeb4c
chavanpranav <chavanvarad514@gmail.com>
2026-01-24 23:31:24 +0530

    Job Schema Done

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -2,18 +2,16 @@ const mongoose = require("mongoose");
 
 const mongoUrl = 'mongodb://127.0.0.1:27017/jobPortalDB';
 
-async function main() {
-
+const connectDB = async () => {
     try{
 
         await mongoose.connect(mongoUrl);
-        console.log("DataBase Connectio Done!");
+        console.log("DataBase Connection Done!");
+
     }catch(err) {
 
         console.log("DataBase Connection Failed!", err);
     }
-}
+};
 
-
-main();
-
+module.exports = connectDB;
--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
+const connectDB = require("./Config/db.js")
+connectDB();
 
+
+const job = require("./Models/job.js");
+/*const testJob = new job({
+    title: "Frontend Developer",
+    company: "XYZ Company",
+    location: "Bangalore",
+    description: "Work on React projects"
+});*/
+
+console.log(testJob);
+
+app.get("/", (req, res) => {
     res.send("This Is Root Page");
 });
 
