5c94966a37c86b5ce37d3610cd183000d6119bb6
Pranav Chavan <chavanvarad514@gmail.com>
2026-06-21 10:30:31 +0530

    now preparing for hosting on vercel + render + mongoDb Atlas

--- Backend/Routes/applicationRoutes.js
+++ Backend/Routes/applicationRoutes.js
@@ -1,25 +1,3 @@
-// import express from "express";
-
-// const router = express.Router();
-
-// import { applyJob, getUserApplications } from "../controllers/applicationController.js";
-// import {authMiddleware} from "../Middleware/auth.js";
-
-// router.post(
-//   "/apply/:jobId",
-//   authMiddleware,
-//   applyJob
-// );
-
-// router.get(
-//   "/me",
-//   authMiddleware,
-//   getUserApplications
-// );
-
-// export default router;
-
-
 import express from "express";
 
 import {
